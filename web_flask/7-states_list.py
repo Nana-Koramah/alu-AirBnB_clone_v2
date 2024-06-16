cffdb50264f907fd186d6cbe61b16829fe923e8f
Nana-Koramah <n.abeasi@alustudent.com>
2024-06-16 13:50:30 +0000

    update

--- web_flask/7-states_list.py
+++ web_flask/7-states_list.py
@@ -1,29 +1,27 @@
 #!/usr/bin/python3
 """
-This module starts a Flask web application that serves a page listing states.
-The states are listed in alphabetical order on the '/states_list' route.
+Flask application module to display a list of states.
 
-The application interacts with a storage system to retrieve and display the states.
-It also ensures the storage is properly closed after each request to maintain resource efficiency.
+This module contains a simple Flask web application that connects to a storage backend,
+retrieves state information, and displays it in a rendered HTML template. It includes
+routes for displaying a list of states and a teardown function to close the storage
+connection after each request.
 """
 
 from flask import Flask, render_template
 from models import *
 from models import storage
 
-# Create an instance of the Flask class
 app = Flask(__name__)
 
 @app.route('/states_list', strict_slashes=False)
 def states_list():
     """
-    Route handler for /states_list.
-    
-    This function retrieves all State objects from the storage, sorts them by name in
-    alphabetical order, and passes the sorted list to an HTML template for rendering.
-    
-    Returns:
-        str: Rendered HTML template displaying the list of states.
+    Display a HTML page with the states listed in alphabetical order.
+
+    This route queries the storage backend for all State objects, sorts them
+    alphabetically by name, and passes them to the '7-states_list.html' template
+    for rendering.
     """
     states = sorted(list(storage.all("State").values()), key=lambda x: x.name)
     return render_template('7-states_list.html', states=states)
@@ -31,16 +29,13 @@ def states_list():
 @app.teardown_appcontext
 def teardown_db(exception):
     """
-    Teardown handler that closes the storage.
-    
-    This function is called automatically after each request to ensure that the
-    database connection is properly closed, which helps in resource management.
-    
-    Args:
-        exception (Exception): The exception that occurred during request handling, if any.
+    Closes the storage on teardown.
+
+    This function is called after each request to close the connection to the
+    storage backend, ensuring that resources are properly released.
     """
     storage.close()
 
 if __name__ == '__main__':
-    # Run the Flask application on the specified host and port
     app.run(host='0.0.0.0', port='5000')
+
